6191a719f9d93e3c987971394d72da91e5468919
spikeUCI <spikeUCI@spikeUCI-PC.(none)>
2012-06-20 22:55:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '56d6a4a538ca56f0c8fe47d3882f323df2bfeed3b4e3e26b7cbf3a49e033fb31ae47a474e61162b48bc0ea0fe5a131de77dc58ae8309e095c39d6f7520533e9a'
